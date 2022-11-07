d48222f55fffd8d5bdbaa23e4d731a0623c28b3f
kimsuyeong <kimsuyeong@localhost>
2022-11-07 23:59:34 +0900

    first commit

---
+++ classicawning/src/main/java/classic/classicawning/ClassicawningApplication.java
@@ -0,0 +1,13 @@
+package classic.classicawning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicawningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicawningApplication.class, args);
+	}
+
+}
---
+++ classicawning/src/test/java/classic/classicawning/ClassicawningApplicationTests.java
@@ -0,0 +1,13 @@
+package classic.classicawning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassicawningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
