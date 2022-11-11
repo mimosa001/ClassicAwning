ae3c7df18c34091891362df386d9e55ad5ce3f7f
kimsuyeong <kimsuyeong@localhost>
2022-11-11 12:33:25 +0900

    작성자 : 김수영 작성일 : 2022-11-11 (금) 작성내용 : 더 보기 클릭 후 글 추가, Controller에 ColorPaletterController(색상표)와 InquiryController(1대1문의) 생성, header에 색상표, 1대1 문의하기 연결

--- classicawning/src/main/java/classic/classicawning/Controller/MainController.java
+++ classicawning/src/main/java/classic/classicawning/Controller/MainController.java
@@ -4,13 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @Controller
-public class UserController {
-	
-	@GetMapping("/mypage/myEstimateList")
-	public String estimateList() {
-		return "/mypage/myEstimateList";
-	}
+public class MainController {
 
+	@GetMapping("/")
+	public String main(Model model) {
+		model.addAttribute("title", "클래식어닝");
+		return "main";
+	}
 }
